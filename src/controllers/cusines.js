361f01db6c4b5c0eb71a23fc109b0b6702217d18
leo <leo42us@gmail.com>
2017-08-23 15:18:53 +0300

    fix cuisine output relates #27

--- src/controllers/cusines.js
+++ src/controllers/cusines.js
@@ -3,8 +3,7 @@ const {findCusines} = require('../models/queries');
 module.exports = (req, res) => {
     findCusines(req.url.split('/')[1])
     .then(cusine => {
-      cusine.unshift({name:req.url.split('/')[1]});
-      res.render('cusine', {cusine})
+      res.render('cusine', {cusine, name:cusine[0].cuisine})
     }
 
     )
