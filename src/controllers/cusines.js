25ddbedff2c09dd5d1f0abf75378b0d5c9784bb7
rinoma <ganzo.s@libero.it>
2017-08-23 12:13:49 +0300

    make first queries and handlebar Relates #25

--- src/controllers/cusines.js
+++ src/controllers/cusines.js
@@ -2,6 +2,11 @@ const {findCusines} = require('../models/queries');
 
 module.exports = (req, res) => {
     findCusines(req.url.split('/')[1])
-    .then(cusine => console.log(cusine))
+    .then(cusine => {
+      cusine.unshift({name:req.url.split('/')[1]});
+      res.render('cusine', {cusine})
+    }
+
+    )
     .catch(err => console.log(err))
 }
--- src/models/queries.js
+++ src/models/queries.js
@@ -1,9 +1,8 @@
 const db = require('../../database/dbConnection')
 
 const findCusines = cusine => {
-  myQuery = `SELECT * FROM recipe WHERE cuisine = $1`;
+  myQuery = `SELECT recipe.id, recipe.title, recipe.difficulty, recipe.duration, recipe.ingredients, recipe.procedure, recipe.cuisine, users.username AS author FROM recipe JOIN users ON recipe.author_id = users.id WHERE recipe.cuisine = $1`;
   //take the cusines from that country
-  console.log(cusine);
   return db.query(myQuery, [cusine]);
 }
 
